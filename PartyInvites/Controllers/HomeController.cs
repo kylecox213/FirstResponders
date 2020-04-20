1952775903f747689978cbdd9b8b87c67b7f7e7a
Carla <astronomykar@gmail.com>
2020-04-20 01:21:46 -0400

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
+
+namespace PartyInvites.Controllers
+{
+	public class HomeController : Controller
+	{
+		private DataContext context;
+
+		public HomeController(DataContext ctx) => context = ctx;
+
+		public IActionResult Index() => View();
+		public IActionResult Respond() => View();
+
+		[HttpPost]
+		public IActionResult Respond(GuestResponse response)
+		{
+			context.Responses.Add(response); 
+				context.SaveChanges();
+				return RedirectToAction(nameof(Thanks),
+					new { Name = response.Name, WillAttend = response.WillAttend });
+			}
+
+			IActionResult Thanks(GuestResponse response) => View(response);
+
+			IActionResult ListResponses() =>
+				View(context.Responses.OrderByDescending(r => r.WillAttend));
+
+	}
+}
