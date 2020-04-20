20a28a4ed66b7f3ee3a4978bab694a1039d8ca42
Carla <astronomykar@gmail.com>
2020-04-20 01:25:59 -0400

    Added comment model

---
+++ PartyInvites/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+	public class Comment
+	{
+		public long Id { get; set; }
+		public string Messsage { get; set; }
+		public bool Pissed { get; set; }
+	}
+}
