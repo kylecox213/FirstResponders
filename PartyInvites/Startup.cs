7dadb35c39785a6c2aa53cc127efe9167a4abb14
Alex <kylecox213@gmail.com>
2020-04-30 23:47:34 -0400

    modified appsettings.json; startup.csproj

--- PartyInvites/Startup.cs
+++ PartyInvites/Startup.cs
@@ -7,34 +7,35 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PartyInvites.Models;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace PartyInvites
 {
 	public class Startup
 	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services)
+		public Startup(IConfiguration config) => Configuration = config;
+
+		public IConfiguration Configuration { get; }
+
+		public void ConfigurationServices(IServiceCollection services)
 		{
+			services.AddMvc();
+			string conString = Configuration["ConnectionStrings:DefaultConnection"];
+			services.AddDbContext<DataContext>(options =>
+			options.UseSqlServer(conString));
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		[Obsolete]
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-
-			app.UseRouting();
-
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapGet("/", async context =>
-				{
-					await context.Response.WriteAsync("Hello World!");
-				});
-			});
+			app.UseDeveloperExceptionPage();
+			app.UseStatusCodePages();
+			app.UseStaticFiles();
+			app.UseMvcWithDefaultRoute();
 		}
+	
 	}
 }
